ac7a8157a36ed96c0b64bbe90f016d6cf30b2407
Vojtech Priban <vojtech.priban@gmail.com>
2025-08-18 12:31:27 +0200

    edited slurm bash script

--- experiment_slurm.sh
+++ experiment_slurm.sh
@@ -7,8 +7,13 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=33
 #SBATCH --gres=gpu:1                  # uncomment if you need GPUs
+echo "SLURM_JOBID = $SLURM_JOBID"
+echo "Running on: $(hostname)"
+echo "Starting at: $(date)"
 
 ml SciPy-bundle/2023.11-gfbf-2023b PyTorch/2.7.0-foss-2023b-CUDA-12.4.0
+echo "Modules loaded"
 source ./quad-swarm-env/bin/activate
+echo "virtual environment sourced"
 # Run your experiment
 python -m sample_factory.launcher.run --run=swarm_rl.runs.quad_multi_mix_modified --max_parallel=1 --pause_between=1 --experiments_per_gpu=1 --num_gpus=1 &
