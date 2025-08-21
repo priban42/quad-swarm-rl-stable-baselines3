7b5a0dee289e796f03fa3b2db77cf899f6c4ebda
Vojtech Priban <vojtech.priban@gmail.com>
2025-08-21 13:43:22 +0200

    fixed launch script dependencies

--- experiment_slurm.sh
+++ experiment_slurm.sh
@@ -12,7 +12,7 @@ echo "Running on: $(hostname)"
 echo "Starting at: $(date)"
 
 #ml SciPy-bundle/2023.11-gfbf-2023b PyTorch/2.7.0-foss-2023b-CUDA-12.4.0
-ml SciPy-bundle/2023.11-gfbf-2023b PyTorch/2.5.0-foss-2023b-CUDA-12.4.0
+ml SciPy-bundle/2023.11-gfbf-2023b PyTorch/2.5.0-foss-2023b-CUDA-12.4.0 typing-extensions/4.14.0-GCCcore-14.2.0
 echo "Modules loaded"
 source ./quad-swarm-env/bin/activate
 echo "virtual environment sourced"
