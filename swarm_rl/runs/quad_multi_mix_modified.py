9c368fad1167bc07e5aad0021cb47075b8f85a88
Vojtech Priban <vojtech.priban@gmail.com>
2025-08-25 14:13:16 +0200

    modified train_for_env_steps

--- swarm_rl/runs/quad_multi_mix_modified.py
+++ swarm_rl/runs/quad_multi_mix_modified.py
@@ -5,7 +5,7 @@ _params = ParamGrid([
 ])
 
 QUAD_BASELINE_CLI_8 = (
-    'python -m swarm_rl.train --env=quadrotor_multi --train_for_env_steps=1000000000 --algo=APPO --use_rnn=False '
+    'python -m swarm_rl.train --env=quadrotor_multi --train_for_env_steps=100000000000 --algo=APPO --use_rnn=False '
     '--num_workers=32 --num_envs_per_worker=4 --learning_rate=0.0001 --ppo_clip_value=5.0 --recurrence=1 '
     '--nonlinearity=tanh --actor_critic_share_weights=False --policy_initialization=xavier_uniform '
     '--adaptive_stddev=False --with_vtrace=False --max_policy_lag=100000000 --rnn_size=256 --with_pbt=False '
